724c16ecb37b01ed2489e727771525ba7b7b7221
Billy Nguyen <bquynhnguyen209@gmail.com>
2024-09-24 14:04:46 -0700

    Implement message protocol for write; TODO Improve it

--- client.c
+++ client.c
@@ -11,7 +11,8 @@
 //   #define MESSAGE_IDENTIFIER "m:"
 //    #define FILE_IDENTIFIER "f:"
 //   #define INDETIFIER_ALLOCATED_SIZE 2
-//   #define FINAL_LENGTH_ALLOCATED_SIZE 4
+#define MESSAGE_LENGTH_ALLOCATED_SIZE 5
+#define WRITE_MESSAGE_IDENTIFIER_SIZE 3
 //    #define TEST_SIZE 13
 //   #define ONE_DIGIT_IDENTIFIER 10
 //   #define TWO_DIGITS_IDENTIFIER 100
@@ -20,7 +21,11 @@
 static int create_socket(void);
 static void display_help_message(int exit_code);
 static void parse_arguments(int argc, char *argv[], int fd);
-static void handle_arguments(int argc, char *argv[], int fd);
+static void handle_write(int argc, char *argv[], int fd);
+static void construct_write_message(char *argv[], size_t len,
+                                    char *final_message);
+// static void prepend_string(char *s, const char *t);
+
 // static void handle_write(char *argv[], int fd);
 //  static void handle_read(int argc, char *argv[], int fd);
 //  static char *process_final_message_length(unsigned long length);
@@ -44,8 +49,7 @@ static void parse_arguments(int argc, char *argv[], int fd) {
       display_help_message(EXIT_SUCCESS);
       break;
     case 'w':
-      handle_arguments(argc, argv, fd);
-      //      handle_write(argv, fd);
+      handle_write(argc, argv, fd);
       break;
       //    case 'r':
       //      handle_arguments(argc, argv, 'r');
@@ -60,100 +64,66 @@ static void parse_arguments(int argc, char *argv[], int fd) {
   }
 }
 
-static void handle_arguments(int argc, char *argv[], int fd) {
+// TODO Fix
+// static void prepend_string(char *s, const char *t) {
+//  size_t len = strlen(t);
+//  memmove(s + len, s, strlen(s) + 1);
+//  memcpy(s, t, len);
+//}
+
+static void construct_write_message(char *argv[], size_t len,
+                                    char *final_message) {
+  char *write_message = malloc(len);
+  char len_string[MESSAGE_LENGTH_ALLOCATED_SIZE];
+  sprintf(len_string, "%zu", len);
+  //  for (size_t i = 0; i < strlen(len_string); i++) {
+  //    prepend_string(len_string, "0");
+  //  }
+  printf("total length as string: %s\n", len_string);
+  strncpy(write_message, len_string, 4);
+  strcat(write_message, ";");
+  strcat(write_message, "w:");
+  strcat(write_message, argv[2]);
+  strcat(write_message, ":");
+  strcat(argv[3], "\0");
+  strcat(write_message, argv[3]);
+
+  strncpy(final_message, write_message, strlen(write_message));
+  //  printf("final message: %s\n", final_message);
+  free(write_message);
+}
+
+static void handle_write(int argc, char *argv[], int fd) {
+  char *final_message;
+  size_t final_message_len;
   for (int i = 0; i < argc; i++) {
     printf("Argument found: %s\n", argv[i]);
   }
 
-  argv[2][strlen(argv[2])] = '\0';
-  write(fd, argv[2], strlen(argv[2]));
-  //  switch (opt) {
-  //  	case 'w':
-  //      if (argc < 5)
-  //	  {
-  //		display_help_message(EXIT_FAILURE);
-  //      }
-  //      break;
-  //    case 'r':
-  //      if (argc < 4)
-  //      {
-  //		display_help_message(EXIT_FAILURE);
-  //      }
-  //      break;
-  //    default:
-  //      display_help_message(EXIT_FAILURE);
-  //      break;
-  //  }
+  if (argc < 4) {
+    display_help_message(EXIT_FAILURE);
+  }
+
+  final_message_len = MESSAGE_LENGTH_ALLOCATED_SIZE // 	"total_length:"
+                      + strlen(argv[2]) +
+                      strlen(argv[3]) //	"f:message:file_location"
+                      + WRITE_MESSAGE_IDENTIFIER_SIZE + 1; // 	"\0"
+  final_message = malloc(final_message_len);
+  construct_write_message(argv, final_message_len, final_message);
+  write(fd, final_message, final_message_len);
+  free(final_message);
 }
-
-// static char *process_final_message_length(unsigned long length) {
-//   char *result = malloc(FINAL_LENGTH_ALLOCATED_SIZE);
-//   char *length_str;
-//
-//   if (length < ONE_DIGIT_IDENTIFIER) {
-//     strncpy(result, "000", 3);
-//     length_str = malloc(1);
-//   }
-//   if (length < TWO_DIGITS_IDENTIFIER) {
-//     strncpy(result, "00", 2);
-//     length_str = malloc(2);
-//   }
-//   if (length < THREE_DIGITS_IDENTIFIER) {
-//     strncpy(result, "0", 1);
-//     length_str = malloc(3);
-//   } else {
-//     length_str = malloc(4);
-//   }
-//   // int to string conversion
-//   sprintf(length_str, "%d", (int)length);
-//   strcat(result, length_str);
-//   printf("Final message length is: %s\n", result);
-//
-//   free(length_str);
-//   return result;
-// }
-
-// static void handle_write(char *argv[], int fd) {
-//   size_t message_len = strlen(argv[2]) + INDETIFIER_ALLOCATED_SIZE;
-//   char *message = malloc(message_len + 1);
-//   size_t final_message_len = FINAL_LENGTH_ALLOCATED_SIZE + message_len + 1;
-//   char *length_allocation = malloc(FINAL_LENGTH_ALLOCATED_SIZE);
-//   char *final_message = malloc(final_message_len);
-//
-//   if (strlen(argv[2]) + 1 >
-//       BUFFER_SIZE - FINAL_LENGTH_ALLOCATED_SIZE - INDETIFIER_ALLOCATED_SIZE)
-//       {
-//     perror("Error: Argument exceeds limit of 1024 characters");
-//   }
-//
-//   // Constructing data (identifer + message)
-//   strncpy(message, MESSAGE_IDENTIFIER, strlen(MESSAGE_IDENTIFIER));
-//   strcat(message, argv[2]);
-//   strcat(message, "\0");
-//   // Constructing final message (length of data + data)
-//   length_allocation = process_final_message_length(message_len + 1);
-//   strncpy(final_message, length_allocation, FINAL_LENGTH_ALLOCATED_SIZE);
-//   strcat(final_message, message);
-//   printf("Final message is: %s\n", final_message);
-//   // Write to socket
-//   write(fd, final_message, final_message_len);
-//
-//   free(final_message);
-//   free(length_allocation);
-//   free(message);
-//}
 
 // static void handle_read(int argc, char *argv[], int fd) {}
 
 _Noreturn static void display_help_message(int exit_code) {
   fprintf(stderr, "Usage: \t./client [options] [target] ...\n");
-  fputs(" \t./client -w <message> -i <filename>\n", stderr);
-  fputs(" \t./client -r -i <filename>\n", stderr);
+  fputs(" \t./client -w <message> <filename>\n", stderr);
+  fputs(" \t./client -r <filename>\n", stderr);
   fputs(" \t./client [-h]\n", stderr);
   fputs("Options:\n", stderr);
-  fputs("\t-w MESSAGE \t\tWrite message to file\n", stderr);
-  fputs("\t-r \t\t\tRead message from file\n", stderr);
-  fputs("\t-i FILE \t\tFile to be written to\n", stderr);
+  fputs("\t-w MESSAGE FILE\t\tWrite message to file\n", stderr);
+  fputs("\t-r FILE\t\t\tRead message from file\n", stderr);
   fputs("\t-h \t\t\tDisplay this help message\n", stderr);
   exit(exit_code);
 }
