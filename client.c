5a4c62044bbd62d24c311a433480dcf8f40bb1af
Billy Nguyen <bquynhnguyen209@gmail.com>
2024-09-25 00:29:12 -0700

    Re-did design and code; TODO Fix failed fopen case, Add error handlings, Cleanup

--- client.c
+++ client.c
@@ -6,124 +6,136 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-#define SOCKET_PATH "/tmp/socket"
-// #define BUFFER_SIZE 1024
-//   #define MESSAGE_IDENTIFIER "m:"
-//    #define FILE_IDENTIFIER "f:"
-//   #define INDETIFIER_ALLOCATED_SIZE 2
-#define MESSAGE_LENGTH_ALLOCATED_SIZE 5
-#define WRITE_MESSAGE_IDENTIFIER_SIZE 3
-//    #define TEST_SIZE 13
-//   #define ONE_DIGIT_IDENTIFIER 10
-//   #define TWO_DIGITS_IDENTIFIER 100
-//   #define THREE_DIGITS_IDENTIFIER 1000
+#define BUFFER_SIZE 1024
+#define ERROR_IDENTIFIER_SIZE 6
+#define SUCCESS_IDENTIFIER_SIZE 8
 
 static int create_socket(void);
 static void display_help_message(int exit_code);
-static void parse_arguments(int argc, char *argv[], int fd);
-static void handle_write(int argc, char *argv[], int fd);
-static void construct_write_message(char *argv[], size_t len,
-                                    char *final_message);
-// static void prepend_string(char *s, const char *t);
+static void process_arguments(int argc, char *argv[], int fd);
+_Noreturn static void process_response(int fd);
+static void handle_arguments(int argc, char *argv[], int fd);
+static void connect_socket(int fd, const char *path) {
+  struct sockaddr_un address;
+  memset(&address, 0, sizeof(address));
+  address.sun_family = AF_UNIX;
+  strncpy(address.sun_path, path, sizeof(address.sun_path) - 1);
+  address.sun_path[sizeof(address.sun_path) - 1] = '\0';
 
-// static void handle_write(char *argv[], int fd);
-//  static void handle_read(int argc, char *argv[], int fd);
-//  static char *process_final_message_length(unsigned long length);
-static void connect_socket(int fd);
+  fd = connect(fd, (struct sockaddr *)&address, sizeof(address));
+  if (fd < 0) {
+    perror("Error: Cannot connect to socket.");
+    exit(EXIT_FAILURE);
+  }
+}
 
 int main(int argc, char *argv[]) {
   int client_fd;
   client_fd = create_socket();
-  connect_socket(client_fd);
-  parse_arguments(argc, argv, client_fd);
+  process_arguments(argc, argv, client_fd);
+  process_response(client_fd);
 
   close(client_fd);
+  fflush(stdout);
   exit(EXIT_SUCCESS);
 }
 
-static void parse_arguments(int argc, char *argv[], int fd) {
+static void process_arguments(int argc, char *argv[], int fd) {
   int opt;
-  while ((opt = getopt(argc, argv, "hwr:")) != -1) {
-    switch (opt) {
-    case 'h':
+
+  if (argc < 3 || argc > 3) {
+    display_help_message(EXIT_FAILURE);
+  }
+
+  if (strlen(argv[1]) > BUFFER_SIZE || strlen(argv[2]) > BUFFER_SIZE) {
+    display_help_message(EXIT_FAILURE);
+  }
+
+  while ((opt = getopt(argc, argv, "h:")) != -1) {
+    if (opt == 'h') {
       display_help_message(EXIT_SUCCESS);
-      break;
-    case 'w':
-      handle_write(argc, argv, fd);
-      break;
-      //    case 'r':
-      //      handle_arguments(argc, argv, 'r');
-      //      break;
-      //    case '?':
-      //      display_help_message(EXIT_SUCCESS);
-      //      break;
-    default: // No arguments
+    } else // No arguments
+    {
       display_help_message(EXIT_FAILURE);
-      break;
+    }
+  }
+  handle_arguments(argc, argv, fd);
+}
+
+_Noreturn static void process_response(int fd) {
+  printf("Response:\n");
+  while (1) {
+    ssize_t bytes;
+    char buffer[BUFFER_SIZE + 1];
+    char error[ERROR_IDENTIFIER_SIZE + 1];
+    char success[SUCCESS_IDENTIFIER_SIZE + 1];
+
+    bytes = recv(fd, &buffer, BUFFER_SIZE, 0);
+    printf("%s", buffer);
+
+    memcpy(error, buffer, ERROR_IDENTIFIER_SIZE);
+    error[ERROR_IDENTIFIER_SIZE] = '\0';
+    memcpy(success, buffer, SUCCESS_IDENTIFIER_SIZE);
+    success[SUCCESS_IDENTIFIER_SIZE] = '\0';
+
+    if (strcmp(error, "\nERROR\0") == 0) {
+      printf("\nExiting with error\n");
+      exit(EXIT_SUCCESS);
+    }
+    if (strcmp(success, "\nSUCCESS\0") == 0) {
+      printf("\nExiting with success\n");
+      exit(EXIT_SUCCESS);
+    }
+    memset(buffer, 0, BUFFER_SIZE);
+    memset(error, 0, ERROR_IDENTIFIER_SIZE + 1);
+    memset(success, 0, SUCCESS_IDENTIFIER_SIZE + 1);
+    fflush(stdout);
+    while (bytes > 0) {
+      bytes = recv(fd, &buffer, BUFFER_SIZE, 0);
+      printf("%s", buffer);
+
+      memcpy(error, buffer, ERROR_IDENTIFIER_SIZE);
+      error[ERROR_IDENTIFIER_SIZE] = '\0';
+      memcpy(success, buffer, SUCCESS_IDENTIFIER_SIZE);
+      success[SUCCESS_IDENTIFIER_SIZE] = '\0';
+
+      if (strcmp(error, "\nERROR\0") == 0) {
+        printf("\nExiting with error\n");
+        exit(EXIT_SUCCESS);
+      }
+      if (strcmp(success, "\nSUCCESS\0") == 0) {
+        printf("\nExiting with success\n");
+        exit(EXIT_SUCCESS);
+      }
+      memset(buffer, 0, BUFFER_SIZE);
+      fflush(stdout);
     }
   }
 }
 
-// TODO Fix
-// static void prepend_string(char *s, const char *t) {
-//  size_t len = strlen(t);
-//  memmove(s + len, s, strlen(s) + 1);
-//  memcpy(s, t, len);
-//}
-
-static void construct_write_message(char *argv[], size_t len,
-                                    char *final_message) {
-  char *write_message = malloc(len);
-  char len_string[MESSAGE_LENGTH_ALLOCATED_SIZE];
-  sprintf(len_string, "%zu", len);
-  //  for (size_t i = 0; i < strlen(len_string); i++) {
-  //    prepend_string(len_string, "0");
-  //  }
-  printf("total length as string: %s\n", len_string);
-  strncpy(write_message, len_string, 4);
-  strcat(write_message, ";");
-  strcat(write_message, "w:");
-  strcat(write_message, argv[2]);
-  strcat(write_message, ":");
-  strcat(argv[3], "\0");
-  strcat(write_message, argv[3]);
-
-  strncpy(final_message, write_message, strlen(write_message));
-  //  printf("final message: %s\n", final_message);
-  free(write_message);
-}
-
-static void handle_write(int argc, char *argv[], int fd) {
-  char *final_message;
-  size_t final_message_len;
+static void handle_arguments(int argc, char *argv[], int fd) {
+  printf("Request:\n");
   for (int i = 0; i < argc; i++) {
-    printf("Argument found: %s\n", argv[i]);
+    printf("%s ", argv[i]);
   }
-
-  if (argc < 4) {
-    display_help_message(EXIT_FAILURE);
+  connect_socket(fd, argv[1]);
+  strcat(argv[2], "\0");
+  if (write(fd, argv[2], strlen(argv[2]) + 1) < 0) {
+    perror("Error: Cannot write to socket.");
   }
-
-  final_message_len = MESSAGE_LENGTH_ALLOCATED_SIZE // 	"total_length:"
-                      + strlen(argv[2]) +
-                      strlen(argv[3]) //	"f:message:file_location"
-                      + WRITE_MESSAGE_IDENTIFIER_SIZE + 1; // 	"\0"
-  final_message = malloc(final_message_len);
-  construct_write_message(argv, final_message_len, final_message);
-  write(fd, final_message, final_message_len);
-  free(final_message);
+  printf("\n");
 }
-
-// static void handle_read(int argc, char *argv[], int fd) {}
 
 _Noreturn static void display_help_message(int exit_code) {
-  fprintf(stderr, "Usage: \t./client [options] [target] ...\n");
-  fputs(" \t./client -w <message> <filename>\n", stderr);
-  fputs(" \t./client -r <filename>\n", stderr);
+  fprintf(stderr, "Usage: \t./client [target] ...\n");
+  fputs(" \t./client <SOCKET_PATH> <FILE>\n", stderr);
   fputs(" \t./client [-h]\n", stderr);
   fputs("Options:\n", stderr);
-  fputs("\t-w MESSAGE FILE\t\tWrite message to file\n", stderr);
-  fputs("\t-r FILE\t\t\tRead message from file\n", stderr);
+  fputs("\t<SOCKET_PATH>\t\tPath to socket (maximum 1024 characters)\n",
+        stderr);
+  fputs("\t<FILE>\t\t\tPath to file and filename (maximum 1024 "
+        "characters)\n",
+        stderr);
   fputs("\t-h \t\t\tDisplay this help message\n", stderr);
   exit(exit_code);
 }
@@ -139,18 +151,4 @@ static int create_socket(void) {
     exit(EXIT_FAILURE);
   }
   return fd;
-}
-
-static void connect_socket(int fd) {
-  struct sockaddr_un address;
-  memset(&address, 0, sizeof(address));
-  address.sun_family = AF_UNIX;
-  strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
-  address.sun_path[sizeof(address.sun_path) - 1] = '\0';
-
-  fd = connect(fd, (struct sockaddr *)&address, sizeof(address));
-  if (fd < 0) {
-    perror("Error: Cannot connect to socket.");
-    exit(EXIT_FAILURE);
-  }
 }
--- server.c
+++ server.c
@@ -5,10 +5,14 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
-#define SOCKET_PATH "/tmp/socket"
+
 #define BUFFER_SIZE 1024
 
 static int create_socket(void);
+static void parse_arguments(int argc, char *argv[], int fd);
+_Noreturn static void process_client_request(int fd);
+// static char* read_data(int fd, char* filename);
+static void display_help_message(int exit_code);
 
 // Create server socket
 static int create_socket(void) {
@@ -26,11 +30,11 @@ static int create_socket(void) {
   return fd;
 }
 
-static void bind_socket(int fd) {
+static void bind_socket(int fd, const char *path) {
   struct sockaddr_un address;
   memset(&address, 0, sizeof(address));
   address.sun_family = AF_UNIX;
-  strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
+  strncpy(address.sun_path, path, sizeof(address.sun_path) - 1);
   address.sun_path[sizeof(address.sun_path) - 1] = '\0';
 
   if (bind(fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
@@ -60,25 +64,83 @@ static int accept_socket(int fd) {
   return client_fd;
 }
 
-int main(void) {
-  int server_fd;
+_Noreturn static void display_help_message(int exit_code) {
+  fprintf(stderr, "Usage: \t./server [target]\n");
+  fputs(" \t./server <SOCKET_PATH>\n", stderr);
+  fputs(" \t./server [-h]\n", stderr);
+  fputs("Options:\n", stderr);
+  fputs("\tSOCKET_PATH\t\tPath to create and link socket to\n", stderr);
+  fputs("\t-h \t\t\tDisplay this help message\n", stderr);
+  exit(exit_code);
+}
 
-  server_fd = create_socket();
-  bind_socket(server_fd);
-  listen_socket(server_fd);
+static void parse_arguments(int argc, char *argv[], int fd) {
+  int opt;
+  if (argc < 2 || argc > 2) {
+    display_help_message(EXIT_FAILURE);
+  }
+
+  while ((opt = getopt(argc, argv, "h:")) != -1) {
+    if (opt == 'h') {
+      display_help_message(EXIT_SUCCESS);
+    } else {
+      display_help_message(EXIT_FAILURE);
+    }
+  }
+  unlink(argv[1]);
+  bind_socket(fd, argv[1]);
+  listen_socket(fd);
+}
+
+_Noreturn static void process_client_request(int fd) {
   while (1) {
     int client_fd;
     ssize_t bytes;
     char buffer[BUFFER_SIZE];
 
-    client_fd = accept_socket(server_fd);
+    client_fd = accept_socket(fd);
     bytes = read(client_fd, &buffer, BUFFER_SIZE);
     while (bytes > 0) {
-      bytes = read(client_fd, &buffer, BUFFER_SIZE);
-      printf("Received %zd bytes.\n", bytes);
+      FILE *file;
+
       printf("Received from client: %s\n", buffer);
+      file = fopen(buffer, "re");
+      memset(buffer, 0, BUFFER_SIZE);
+      printf("fopen is %s\n", (file == NULL) ? "NULL" : "SUCCESS");
+      if (file == NULL) {
+        printf("Reponse: ERROR opening file.");
+        bytes = write(client_fd, "\nERROR - Fail to open file.\0",
+                      strlen("\nERROR - Fail to open file.\0"));
+        if (bytes < 0) {
+          perror("Error: Cannot write to file.");
+        }
+      } else {
+        while (fgets(buffer, sizeof(buffer), file) != NULL) {
+          printf("%s", buffer);
+          bytes = send(client_fd, buffer, strlen(buffer), 0);
+          memset(buffer, 0, BUFFER_SIZE);
+        }
+        fclose(file);
+        write(client_fd, "\nSUCCESS - File read successfully.\0",
+              strlen("\nSUCCESS - File read successfully.\0"));
+        printf("\n");
+        printf("Reponse: File read successfully.\n");
+      }
+      break;
+    }
+    if (bytes < 0) {
+      perror("Error: Cannot read from socket.");
     }
   }
+}
+
+int main(int argc, char *argv[]) {
+  int server_fd;
+
+  server_fd = create_socket();
+  parse_arguments(argc, argv, server_fd);
+  process_client_request(server_fd);
+
   close(server_fd);
   exit(EXIT_SUCCESS);
 }
